8767c1291ed08dcdd83c92b4d21f6d1f80759dc3
Mehdi <mehdi-bs@live.fr>
2019-12-12 19:50:24 +0100

    Update utils.py
    verbose

--- Bayesian_hmm/utils.py
+++ Bayesian_hmm/utils.py
@@ -1,13 +1,20 @@
 import numpy as np
 
-def assign_classes(series, models):
+def assign_classes(series, models, verbose=False):
     """ models must be a list of bayesian_hmm fitted object"""
     predicted_labels = []
+    if verbose:
+        print("-----")
+        print("Assigning class to series")
+        print("-----")
     for new_series in series:
         probas = []
         for m in models:
             probas.append((m.likelihood(new_series), m.class_label))
         probas = sorted(probas, key=lambda x : x[0])
+        if verbose:
+            print(probas)
+            print("----")
         predicted_labels.append(probas[-1][1])
 
     return np.array(predicted_labels)
