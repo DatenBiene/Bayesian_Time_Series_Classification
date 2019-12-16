96445c1928bc121bf9598ee0ade24929cb2d70b1
Mehdi <mehdi-bs@live.fr>
2019-12-16 10:19:15 +0100

    Add files via upload

---
+++ Bayesian_hmm/__init__.py
@@ -0,0 +1,22 @@
+""" Module for time series classification using Bayesian Hidden Markov Model 
+    -----------------
+    Version : 0.1 
+    Date : December, 11th 2019
+    Authors : Pierre Delanoue & Mehdi Bennaceur 
+    Phase : Development 
+    Contact : _
+    Github : https://github.com/DatenBiene/Bayesian_Time_Series_Classification
+"""
+
+__version__ = "0.2"
+__date__ = "December, 11th 2019"
+__author__ = 'Mehdi Bennaceur'
+__github__ = "https://github.com/DatenBiene/Bayesian_Time_Series_Classification"
+
+from Bayesian_hmm.bayes_hmm import bayesian_hmm 
+from Bayesian_hmm.simulate_data import generate_markov_seq, generate_transtion_matrix, generate_series, generate_samples
+from Bayesian_hmm.utils import assign_classes
+
+
+__all__ = ["bayesian_hmm", "generate_markov_seq", "generate_transtion_matrix", "generate_series", "generate_samples",
+          "assign_classes"]
