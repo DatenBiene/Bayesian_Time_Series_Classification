634f122004ba8338ec26ecf6bfb6977efc9e6556
Mehdi <mehdi-bs@live.fr>
2019-12-16 16:09:09 +0100

    Update utils.py

--- Bayesian_hmm/utils.py
+++ Bayesian_hmm/utils.py
@@ -18,3 +18,23 @@ def assign_classes(series, models, verbose=False):
         predicted_labels.append(probas[-1][1])
 
     return np.array(predicted_labels)
+
+
+def build_hmm_models(X_data, n_model, n_states, hmm_obj, n_iter_gibbs=2000, max_obs = None):
+    print("Building", n_model, " Bayesian HMM instances with", n_states, "hidden states ...")
+    #create priors 
+    if max_obs is None:
+        max_obs = 900
+    models = []
+    for classe in range(1, n_model + 1):
+        print("------")
+        print("Fitting Class", classe, "...")
+        #sns.distplot(np.concatenate(X_data[classe])[:max_obs], hist=False)
+        #plt.show()
+        prior_p, prior_transitions = {}, np.ones((n_states, n_states))
+        for s in range(n_states):
+            prior_p[s] = [0, 1, 1, 2]
+        model_class_step = hmm_obj(prior_p, prior_transitions, n_iter_gibbs, class_label=classe)
+        model_class_step.fit(np.concatenate(X_data[classe])[:max_obs])
+        models.append(model_class_step)
+    return models
